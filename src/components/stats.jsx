df68b8ba28dfe07b2811225a8e4a63ce9b0c4f3d
Tejendrasinh6844 <149938061+Tejendrasinh6844@users.noreply.github.com>
2024-12-16 23:10:07 -0800

    update more features

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -1,7 +1,64 @@
+import { BsCashCoin, BsCurrencyDollar } from "react-icons/bs"
+import { IoMdArrowDown, IoMdArrowUp } from "react-icons/io"
+import { SiCashapp } from "react-icons/si"
 
-const Stats = () => {
+
+  const data = [
+    {
+      lable:"Your Total Balance",
+      amount:"10,000.00",
+      increase: 10.9,
+      icon:<BsCurrencyDollar/>
+    },
+    {
+      lable:"Total Income",
+      amount:"15,000.00",
+      icon:<BsCashCoin size={26}/>,
+      increase: 10.9,
+    },
+    {
+      lable:"Total Expense",
+      amount:"10,452.00",
+      increase: -10.9,
+      icon:<SiCashapp size={26}/>
+    }  ]
+
+    const ICON_STYLES = [
+      "bg-blue-300 text-blue-800",
+      "bg-emerald-300 text-emerald-800",
+      "bg-rose-300 text-rose-800"
+    ]
+
+
+
+    const Stats = () => {
   return (
-    <div>Stats</div>
+    <div className="flex flex-col md:flex-row items-center justify-between gap-8 2xl:gap-40 mb-20 ">
+      {
+        data.map((item, index)=>(
+       <div key={index + item.lable} className="w-full 2xl:min-w-96 flex items-center justify-between gap-5 px-4 md:px-8 py-12 rounded-lg bg-gray-50 dark:bg-slate-800 border border-gray-100 dark:border-slate-900">
+        <div className="flex items-center gap-2">
+         <div className={`w-12 h-12 flex items-center justify-center rounded-full ${ICON_STYLES[index]}`}>
+          {item.icon} 
+          </div>
+
+         <div className=" space-y-3">
+          <span className="text-gray-600 dark:text-gray-400 text-base md:text-lg">{item.lable}</span>
+          <p className="text-2xl 2xl:text-3xl font-medium text-black dark:text-gray-400">%{item.amount}</p>
+         </div>
+
+        </div>
+
+        <div>
+<p className={`flex gap-1 items-center text-base md:text-lg font-semibold ${item.increase > 0 ? "text-emerald-500" : "text-red-500"}`}>
+   {item.increase > 0 ? <IoMdArrowUp/> : <IoMdArrowDown/> }
+  {Math.abs(item.increase)}%</p>
+<span className="text-xs md:text-sm text-gray-600 dark:text-gray-500">Compare to last year</span>
+        </div>
+       </div>
+        ))
+      }
+    </div>
   )
 }
 
